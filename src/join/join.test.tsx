786441e383e732939d885fc43fdb2cb0da07320b
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-02-28 20:59:25 +0100

    Adds single minor unit test

--- src/join/join.test.tsx
+++ src/join/join.test.tsx
@@ -14,4 +14,17 @@ describe("Join component", () => {
 
     expect(getAllByText(/mock separator/)).toHaveLength(1);
   });
+
+  it("handles mapped children", () => {
+    const contentArray = ["foo", "bar", "buzz"];
+    const { getAllByText } = render(
+      <Join separator={<span>mock separator</span>}>
+        {contentArray.map((content, index) => (
+          <span key={index}>{content}</span>
+        ))}
+      </Join>
+    );
+
+    expect(getAllByText(/mock separator/)).toHaveLength(2);
+  });
 });
