a2f30826b97fc18b96f92ba9d6913400ab233178
Sam Collard <github@swifteagle.co.uk>
2021-03-14 14:42:34 +0000

    Use union type in useDataSource state

--- src/hooks/useDataSource/useDataSource.tsx
+++ src/hooks/useDataSource/useDataSource.tsx
@@ -1,49 +1,38 @@
 import * as React from "react";
 
-type Refreshable = { refresh: () => void };
-
 type DataSuccess<T> = {
   state: "success";
   data: T;
-} & Refreshable;
+};
 
 type DataError = {
   state: "error";
-} & Refreshable;
+};
 
 type DataLoading = {
   state: "loading";
-} & Refreshable;
+};
 
 export type DataResult<T> = DataSuccess<T> | DataError | DataLoading;
 
-export function useDataSource<T>(dataSource: () => Promise<T>): DataResult<T> {
-  const [data, setData] = React.useState<T | undefined>();
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState(false);
+export function useDataSource<T>(
+  dataSource: () => Promise<T>
+): DataResult<T> & { refresh: () => void } {
+  const [data, setData] = React.useState<DataResult<T>>({
+    state: "loading",
+  });
+
   const refresh = React.useCallback(() => {
-    setLoading(true);
-    setError(false);
+    setData({
+      state: "loading",
+    });
+
     dataSource()
-      .then((newData) => setData(newData))
-      .catch(() => setError(true)) // TODO do something with the caught error?
-      .finally(() => setLoading(false));
+      .then((newData) => setData({ state: "success", data: newData }))
+      .catch(() => setData({ state: "error" }));
   }, []);
 
   React.useEffect(refresh, []);
 
-  if (error) {
-    return { state: "error", refresh };
-  }
-
-  if (loading) {
-    return { state: "loading", refresh };
-  }
-
-  if (data) {
-    return { state: "success", data, refresh };
-  }
-
-  // TODO refactor the above so that this line is not necessary
-  throw new Error("This shouldn't be possible");
+  return { ...data, refresh };
 }
