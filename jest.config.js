e5b9bcc3b4d31e44eaeb7bf099becb3b866b4c33
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-03-14 11:31:56 +0100

    Basic test case

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  clearMocks: true,
   collectCoverageFrom: ["src/**", "!src/**/*.stories.tsx"],
   modulePathIgnorePatterns: ["dist"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
 };
--- src/hooks/useDataSource/useDataSource.test.ts
+++ src/hooks/useDataSource/useDataSource.test.ts
@@ -1,1 +1,20 @@
-// TODO
+import { renderHook } from "@testing-library/react-hooks";
+import { useDataSource } from "./useDataSource";
+
+describe("useDataSource", () => {
+  const mockDataSource = jest.fn().mockResolvedValue("mock data");
+
+  it("calls the dataSource on mount", async () => {
+    const { waitForNextUpdate } = renderHook(() =>
+      useDataSource(mockDataSource)
+    );
+
+    await waitForNextUpdate();
+
+    expect(mockDataSource).toHaveBeenCalled();
+  });
+
+  it.todo("error case");
+
+  it.todo("loading case");
+});
