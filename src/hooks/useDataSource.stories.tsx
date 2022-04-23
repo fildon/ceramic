a1c4932c7e4d4fde36c83987d12a4e8dc5fe0794
CheshireSwift <git@swifteagle.co.uk>
2022-04-23 18:28:45 +0100

    📚 Refactor useDataSource stories for Show Code

--- src/hooks/useDataSource.stories.tsx
+++ src/hooks/useDataSource.stories.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import { Story, Meta } from "@storybook/react";
 import { DataResult, useDataSource } from "./useDataSource";
 
-const dataSourceThatSucceeds = () =>
-  new Promise<string>((resolve) => {
-    setTimeout(() => resolve("you got the data!"), 1000);
+const wait = (ms: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(resolve, ms);
   });
 
+const dataSourceThatSucceeds = () =>
+  wait(1000).then(() => Promise.resolve("you got the data!"));
 const dataSourceThatFails = () =>
-  new Promise<string>((_resolve, reject) => {
-    setTimeout(() => reject("Your internet is down"), 1000);
-  });
+  wait(1000).then(() => Promise.reject("Your internet is down"));
 
 interface DisplayDataResultProps {
   dataResult: DataResult<string>;
@@ -30,43 +30,51 @@ interface DemoUseDataSourceProps {
   dataSource: () => Promise<string>;
   message: string;
 }
-const DemoUseDataSource = ({ dataSource, message }: DemoUseDataSourceProps) => {
-  const dataResult = useDataSource(() => dataSource());
-
-  return (
-    <>
-      <p>
-        This component is simply a harness for demonstrating the useDataSource
-        hook. Below a data display is mounted with the hook, which after about
-        one second will <em>{message}</em>.
-      </p>
-      <p>
-        The hook returns an object containing:
-        <ul>
-          <li>
-            State: a state enum indicating one of Error, Loading, Success.
-          </li>
-          <li>Data: if and only if the state is successful.</li>
-          <li>
-            Refresh: a callback which can be invoked to refetch the data from
-            the source.
-          </li>
-        </ul>
-      </p>
-      <DisplayDataResult dataResult={dataResult} />
-      <button onClick={dataResult.refresh}>Click me to force a refetch.</button>
-    </>
-  );
-};
-
 export default {
   title: "Hooks/useDataSource",
-  component: DemoUseDataSource,
 } as Meta;
 
-const Template: Story<DemoUseDataSourceProps> = (args) => (
-  <DemoUseDataSource {...args} />
-);
+const Template: Story<DemoUseDataSourceProps> = (args) => {
+  const DemoUseDataSource = ({
+    dataSource,
+    message,
+  }: DemoUseDataSourceProps) => {
+    const dataResult = useDataSource(() => dataSource());
+
+    return (
+      <>
+        <p>
+          This component is simply a harness for demonstrating the useDataSource
+          hook. Below a data display is mounted with the hook, which after about
+          one second will <em>{message}</em>.
+        </p>
+        <p>
+          The hook returns an object containing:
+          <ul>
+            <li>
+              State: a state enum indicating one of Error, Loading, Success.
+            </li>
+            <li>Data: if and only if the state is successful.</li>
+            <li>
+              Refresh: a callback which can be invoked to refetch the data from
+              the source.
+            </li>
+          </ul>
+        </p>
+        <p>
+          Click on <code>Show code</code> in the Docs tab to see how this
+          example works.
+        </p>
+        <DisplayDataResult dataResult={dataResult} />
+        <button onClick={dataResult.refresh}>
+          Click me to force a refetch.
+        </button>
+      </>
+    );
+  };
+
+  return <DemoUseDataSource {...args} />;
+};
 
 export const DataSourceSuccess = Template.bind({});
 DataSourceSuccess.args = {
